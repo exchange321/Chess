7d3d1ea529f95994ba4f1e0498730fbf2ab6b756
jason <hieutran106@gmail.com>
2017-06-23 15:50:22 +1000

    add redux partly

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import React from 'react'
 import { render } from 'react-dom';
@@ -8,5 +10,7 @@ import AppEntry from './AppEntry.jsx';
 window.jQuery = window.$=require('jquery');
 window.Tether = require('tether');
 require('bootstrap');
+
+injectTapEventPlugin();
 
 render(React.createElement(AppEntry), document.querySelector("#app"));
