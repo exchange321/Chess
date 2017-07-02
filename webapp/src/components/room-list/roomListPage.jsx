b1bb135d94653ea226468cab870843d39f59c40f
jason <hieutran106@gmail.com>
2017-07-02 15:28:47 +1000

    add createRoom modal

--- webapp/src/components/room-list/roomListPage.jsx
+++ webapp/src/components/room-list/roomListPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Col,Label,Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'; 
 import { connect } from 'react-redux'
 import Room from './Room.jsx'
 
 class RoomList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            modal: false
+        };
     }
    
     render() {
@@ -25,13 +29,33 @@ class RoomList extends React.Component {
                         <Room key={room.id} {...room} onClick={this.joinRoomClick}/>
                     ))}
                 </div>
-                <button type="button" className="btn btn-default" onClick={this.createRoomClick}>CREATE ROOM</button>
+                <button type="button" className="btn btn-default" onClick={this.toggleModal}>CREATE ROOM</button>
+                <Modal isOpen={this.state.modal} toggle={this.toggleModal} className="create-room-modal">
+                    <ModalHeader toggle={this.toggle}>Create a room</ModalHeader>
+                    <ModalBody>
+                        <Label sm={40}>Enter a name:</Label>
+                        <Col sm={10}>
+                            <Input type="text" name="Enter a name:" id="roomName" placeholder="Room 1" />
+                        </Col>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.createRoomClick}>Create a room</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
                 
             </div>
         )
     }
-    createRoomClick = (e) => {
-        console.log("create room click");
+    toggleModal = (e) => {
+        //toggle the modal
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+    createRoomClick=(e) => {
+        console.log("create a room, name=");
+
     }
     
     joinRoomClick = (roomId) => {
