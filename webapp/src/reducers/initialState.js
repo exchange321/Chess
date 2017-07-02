636f180ff17d697baa336958021af22debed4ca1
jason <hieutran106@gmail.com>
2017-07-02 15:37:37 +1000

    add RoomDetailPage

--- webapp/src/reducers/initialState.js
+++ webapp/src/reducers/initialState.js
@@ -10,5 +10,24 @@ export default {
             {id:2,name:"Room 2",numPlayer:2,status:"Waiting..."},
             {id:3,name:"Room 3",numPlayer:2,status:"Playing..."}
         ],
+    },
+    roomDetailPage: {
+        info: {
+            id:1,
+            roomName:"Room 1"
+        },
+        slots: [
+            {
+                id:0,
+                name:"player 1",
+                status:0
+            },
+            {
+                id:1,
+                name:"computer insane",
+                status:0,
+            }
+        ]
+        
     }
 }
--- webapp/src/reducers/rootReducer.js
+++ webapp/src/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import {combineReducers} from 'redux'
 import loginReducer from './loginReducer'
 import roomListReducer from './roomListReducer'
-
+import roomDetailReducer from './roomDetailReducer'
 const rootReducer=combineReducers(
     {
         loginPage: loginReducer,
-        roomListPage: roomListReducer
+        roomListPage: roomListReducer,
+        roomDetailPage:roomDetailReducer
     }
 )
 
