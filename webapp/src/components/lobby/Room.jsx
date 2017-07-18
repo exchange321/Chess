560237fd04aac4cf3d65aa1ecf61560220a8fd7f
jason <hieutran106@gmail.com>
2017-07-18 10:00:38 +1000

    edit styling in RoomPage

--- webapp/src/components/lobby/Room.jsx
+++ webapp/src/components/lobby/Room.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FontIcon from 'material-ui/FontIcon';
-import { blue500, greenA200 } from 'material-ui/styles/colors';
-import RaisedButton from 'material-ui/RaisedButton';
 
 // eslint-disable-next-line no-unused-vars
 const Room = ({ id, name, owner, numPlayer }) => (
-  <div className="room" >
-    <div className="name-wrapper" >
-      <div className="room-name">{name}</div>
-      <div className="owner-name"><strong>Owner:</strong> {owner.playerName}</div>
-    </div>
-    <div className="icon-btn-wrapper" >
-      <div className="join-btn" >
-        <RaisedButton
-          type="submit"
-          label="Join"
-          primary
-          icon={<FontIcon
-            className="fa fa-sign-in"
-          />}
-          fullWidth
-        />
+  <div className="room row" >
+    <div className="room-info col-xl-9">
+      <div className="row">
+        <div className="room-avatar col-xl-2"><div className="avatar-img" /></div>
+        <div className="name-wrapper col-xl-10" >
+          <div className="room-name"><p>{name}</p></div>
+          <div className="owner-name"><p>{owner.playerName}</p></div>
+        </div>
       </div>
+    </div>
+    <div className="icon-btn-wrapper col-xl-3" >
       <div className="icon-player">
-        <FontIcon
-          className="fa fa-user fa-2x"
-          color={blue500}
-        />
-        <FontIcon
-          className="fa fa-user fa-2x"
-          color={numPlayer === 1 ? greenA200 : blue500}
-        />
+        <i className="fa fa-user fa-2x" />
+        <i className="fa fa-user fa-2x" />
+      </div>
+      <div className="join-btn">
+        <button className="btn btn-secondary" type="submit">Join</button>
       </div>
     </div>
   </div>
